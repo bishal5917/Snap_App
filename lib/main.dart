1a598b011b6406b16c663e34ea50a54ea5bbba77
bishal5917 <poudelb172@gmail.com>
2022-10-10 15:54:45 +0545

    using provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:snap_app/places_list_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:snap_app/providers/great_places.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: PlacesListScreen(),
       ),
-      home: PlacesListScreen(),
     );
   }
 }
